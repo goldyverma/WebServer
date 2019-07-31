5289bf2b36821ace84ecba8d8ac88cba48ca9031
Goldy Verma <749947@student.tor.loc>
2019-07-31 15:36:35 -0400

    fifth commit at 3:36pm

--- purenodeserver.js
+++ purenodeserver.js
@@ -1,7 +1,17 @@
 const http = require('http');
+const url = require('url');
 
 function handler(req, res){
+
+    const parseUrl = url.parse(req.url, true);
+    console.log(parseUrl);
+
     res.writeHead(200,{'Content-type':'text/plain'});
+    res.write('Hello, I am a webserver!');
+    res.end();
 }
 
-const server = http.createServer();
+const server = http.createServer(handler);
+
+server.listen(3000);
+
